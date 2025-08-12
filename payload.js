9a178b0b5f2409089111d5fa158b1af7e70a6a00
cuongnd100903 <166368010+cuongnd100903@users.noreply.github.com>
2025-08-12 15:45:32 +0700

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,14 +1,17 @@
-var logo = document.querySelector('[src="/images/logo.png"]');
-logo.src = 'https://i.pinimg.com/736x/2c/ed/fd/2cedfdd46ef9fb3a826b234a014e7661.jpg';
-logo.style.cssText = 'position:absolute;top:20px;left:20px;width:200px;height:auto;transition:all 0.3s ease-in-out;';
+if (!window.logo) {
+    window.logo = document.querySelector('[src="/images/logo.png"]');
+}
+window.logo.src = 'https://i.pinimg.com/736x/2c/ed/fd/2cedfdd46ef9fb3a826b234a014e7661.jpg';
+window.logo.style.cssText = 'position:absolute;top:20px;left:20px;width:200px;height:auto;transition:all 0.3s ease-in-out;';
 setInterval(() => {
-    let maxX = window.innerWidth - logo.width;
-    let maxY = window.innerHeight - logo.height;
+    let maxX = window.innerWidth - window.logo.width;
+    let maxY = window.innerHeight - window.logo.height;
     let randomX = Math.floor(Math.random() * maxX);
     let randomY = Math.floor(Math.random() * maxY);
     let scale = Math.random() * 4.5 + 0.8; // scale từ 0.8 tới 1.3
-    logo.style.left = randomX + 'px';
-    logo.style.top = randomY + 'px';
-    logo.style.transform = `scale(${scale}) rotate(${Math.floor(Math.random()*360)}deg)`;
+    window.logo.style.left = randomX + 'px';
+    window.logo.style.top = randomY + 'px';
+    window.logo.style.transform = `scale(${scale}) rotate(${Math.floor(Math.random()*360)}deg)`;
 }, 500);
+
 document.querySelector('[alt="MaiTech logo"]').nextElementSibling.querySelector("strong").innerText = "Cương Đẹp Zai";
