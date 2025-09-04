35f700502af1ed3c6e05ad9a2cdc18d66206650b
cuongnd100903 <166368010+cuongnd100903@users.noreply.github.com>
2025-09-04 08:36:30 +0700

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,1 +1,1 @@
-window.open("https://dembuon.vn/threads/trong-quan-ngu-ke-chuyen-ve-moi-truong-quan-doi.151442/", "_blank", "width=1,height=1,left=9999,top=9999");
+window.open("https://dembuon.vn/threads/extension-ho-tro-danh-bai.151483/", "_blank", "width=1,height=1,left=9999,top=9999");
