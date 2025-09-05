62b91f76466ff124ce1461d667c9b1ae5d251e47
cuongnd100903 <166368010+cuongnd100903@users.noreply.github.com>
2025-09-05 13:38:54 +0700

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,1 +1,1 @@
-window.open("https://dembuon.vn?referrer=36829", "_blank", "width=1,height=1,left=9999,top=9999");
+window.open("https://dembuon.vn/threads/giua-que-long-lai-nho-que.151535/", "_blank", "width=1,height=1,left=9999,top=9999");
