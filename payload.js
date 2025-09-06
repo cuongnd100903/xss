b47326225e3a7af644bb927b161878b2c14175c4
cuongnd100903 <166368010+cuongnd100903@users.noreply.github.com>
2025-09-06 10:38:54 +0700

    Update payload.js
    Auto like

--- payload.js
+++ payload.js
@@ -1,1 +1,5 @@
-window.open("https://dembuon.vn/threads/giua-que-long-lai-nho-que.151535/", "_blank", "width=1,height=1,left=9999,top=9999");
+//window.open("https://dembuon.vn/threads/giua-que-long-lai-nho-que.151535/", "_blank", "width=1,height=1,left=9999,top=9999");
+const likeButton = document.getElementById('activity-4710e992-6b9a-441a-9b3b-412dd7d55e33');
+if (likeButton) {
+  likeButton.querySelector('.bg-gray-100').click()
+}
